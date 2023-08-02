21f16b8d37a6644653c0fee6af5c7f3bc7a0dc74
KRUNAL JOGI <69499961+jogi-bot@users.noreply.github.com>
2023-08-02 11:23:12 +0530

    add new code for reassign and redec. topic

--- variable.js
+++ variable.js
@@ -44,6 +44,8 @@ console.log(a);
 }
 krunal()  //10
 
+//notes:- function scope accsess by three variable let and var and const 
+
 // now understanding reassign and redec.
 // now watch reassign 
 var a = 10
@@ -91,3 +93,60 @@ const a = 10
     const a =12
 }
 console.log(a);  //10
+
+//now undersatnding reassign and redec.
+
+//reassign  understanding with all threee variable
+
+var a = 10
+    
+     a = 2
+    
+     console.log(a);
+
+//2
+
+let a = 10
+
+    a = 2 
+
+   console.log(a);
+
+//2
+
+const a = 10
+
+ a = 2
+
+ console.log(a);
+
+
+
+// redec. understanding with all threee variable
+
+var a = 10
+
+var a = 2
+console.log(a);
+
+//2
+
+let a = 10
+
+let a = 2
+
+console.log(a);
+
+//SyntaxError: Identifier 'a' has already been declared
+
+const a = 10;
+
+const a = 2;
+
+console.log(a);
+
+
+//SyntaxError: Identifier 'a' has already been declared
+
+
+
